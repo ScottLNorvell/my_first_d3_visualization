04306569a2364b62112e94d5da9398bda6e0d06f
Scott Norvell <scottlnorvell@gmail.com>
2013-10-30 00:04:14 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# My First d3 Visualization
+
+#### You always remember your first...
+
+This code is based on OMG particles found [here](http://bl.ocks.org/mbostock/1062544).
+
+In this exercise, I played around with rendering multiple particles. The result started looking like smoke, so I altered the colors to match, and randomized the dispersion. I thought it looked a little like the Smoke Monster from Lost, so I changed the background to green...
+
+
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+// based on OMG particles found here:
+// http://bl.ocks.org/mbostock/1062544
+var w = window.innerWidth,
+    h = window.innerHeight,
+    z = d3.scale.category10(),
+    i = 0;
+
+var svg = d3.select("body").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h)
+    .style("pointer-events", "all")
+    .on("mousemove", particle);
+
+var blacks = ["#242424", "#1B1B1B", "#1A1110", "#222222"];
+var duration = 4000;
+var grow = 65;
+function particle() {
+  var m = d3.mouse(this);
+
+  svg.append("svg:circle")
+      .attr("cx", m[0])
+      .attr("cy", m[1])
+      .attr("r", 1e-6)
+      // .style("stroke", "#111111")
+      .style("fill", blacks[0])
+      .style("stroke-opacity", 1)
+    .transition()
+      .duration(duration)
+      .ease(Math.sqrt)
+      .attr("r", grow)
+      .attr("cx", m[0]-Math.random() * 100)
+      .style("stroke-opacity", 1e-6)
+      .style("fill-opacity", 1e-6)
+      .remove();
+  
+  svg.append("svg:circle")
+      .attr("cx", m[0])
+      .attr("cy", m[1])
+      .attr("r", 1e-6)
+      // .style("stroke", "#111111")
+      .style("fill", blacks[1])
+      .style("stroke-opacity", 1)
+    .transition()
+      .duration(duration)
+      .ease(Math.sqrt)
+      .attr("r", grow)
+      .attr("cx", m[0]+Math.random() * 100)
+      .style("stroke-opacity", 1e-6)
+      .style("fill-opacity", 1e-6)
+      .remove();
+
+  svg.append("svg:circle")
+      .attr("cx", m[0])
+      .attr("cy", m[1])
+      .attr("r", 1e-6)
+      // .style("stroke", "#111111")
+      .style("fill", blacks[2])
+      .style("stroke-opacity", 1)
+    .transition()
+      .duration(duration)
+      .ease(Math.sqrt)
+      .attr("r", grow)
+      .attr("cy", m[1]-Math.random() * 100)
+      .style("stroke-opacity", 1e-6)
+      .style("fill-opacity", 1e-6)
+      .remove();
+  
+  svg.append("svg:circle")
+      .attr("cx", m[0])
+      .attr("cy", m[1])
+      .attr("r", 1e-6)
+      // .style("stroke", "#111111")
+      .style("fill", blacks[3])
+      .style("stroke-opacity", 1)
+    .transition()
+      .duration(duration)
+      .ease(Math.sqrt)
+      .attr("r", grow)
+      .attr("cy", m[1]+Math.random() * 100)
+      .style("stroke-opacity", 1e-6)
+      .style("fill-opacity", 1e-6)
+      .remove();
+}
